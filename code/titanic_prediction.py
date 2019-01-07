a479a5fd71a919d8e814513ce23839292533779b
giancarloyona <gianyona@gmail.com>
2019-01-07 17:50:49 -0200

    generated the first model

--- code/titanic_prediction.py
+++ code/titanic_prediction.py
@@ -4,19 +4,16 @@
     This is a starter's attempt to generate a survival predictor using 
     Titanic's sinking data.
     
-    This model will use the Decision Tree Regression as the prediction
+    This first model will use the Decision Tree Regression as the prediction
     algorithm.
     
     @author: giancarloyona
     @email: gianyona[at]gmail.com
     @date: jan/19
-
-    TO DO: perform the preprocessing steps on the test dataset
-
+    @version: 0.1
 """
 
 # importing the dataset
-
 import numpy as np
 import pandas as pd
 
@@ -24,15 +21,15 @@ training_set = pd.read_csv('train.csv')
 test_set = pd.read_csv('test.csv')
 
 # preprocessing the data
-X_train = training_set.iloc[:, [2,4,5,6,7,11]].values
+X_train = training_set.iloc[:, [2, 4, 5, 6, 7, 11]].values
 Y_train = training_set.iloc[: , 1].values
 Y_train = np.reshape(Y_train, (-1, 1))
 
-test_set = test_set.iloc[:, [1,3,4,5,6,10]].values
+test_set = test_set.iloc[:, [1, 3, 4, 5, 6, 10]].values
 
 ## replacing missing values
 
-### replacing NaN to string values
+### replacing null values
 X_train[pd.isnull(X_train)] = 'NaN'
 
 ### replacing numeric values
@@ -42,26 +39,51 @@ imputer_training = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer_training  = imputer_training.fit(X_train[:, [0, 2, 3, 4]])
 X_train[:, [0, 2, 3, 4]] = imputer_training.transform(X_train[:, [0, 2, 3, 4]])
 
+imputer_training_1 = SimpleImputer(missing_values='NaN', strategy='most_frequent')
+imputer_training_1 = imputer_training_1.fit(X_train[:, [1, 5]])
+X_train[: , [1, 5]] = imputer_training_1.transform(X_train[:, [1, 5]])
+
 imputer_test = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer_test = imputer_test.fit(training_set[: , [0,2,3,4]])
-training_set[: , [0,2,3,4]] = imputer_test.transform(training_set[: , [0,2,3,4]])
+imputer_test = imputer_test.fit(test_set[: , [0, 2, 3, 4]])
+test_set[: , [0, 2, 3, 4]] = imputer_test.transform(test_set[: , [0, 2, 3, 4]])
+
+imputer_test_1 = SimpleImputer(missing_values='NaN', strategy='most_frequent')
+imputer_test_1 = imputer_test_1.fit(test_set[:, [1, 5]])
+test_set[: , [1, 5]] = imputer_test_1.transform(test_set[:, [1, 5]])
 
 ## encoding the variables
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-label_encoder = LabelEncoder()
-X_train[:, 1] = label_encoder.fit_transform(X_train[:, 1])
-X_train[:, 5] = label_encoder.fit_transform(X_train[:, 5])
+### encoding the train data
+label_encoder_train = LabelEncoder()
+X_train[:, 0] = label_encoder_train.fit_transform(X_train[:, 0])
+X_train[:, 1] = label_encoder_train.fit_transform(X_train[:, 1])
+X_train[:, 5] = label_encoder_train.fit_transform(X_train[:, 5])
 
-column_transform = ColumnTransformer([
-        ('Pclass', OneHotEncoder(categories='auto'), [0]),
-        ('Sex', OneHotEncoder(categories='auto'), [1]),
-        ('Embarked', OneHotEncoder(categories='auto'), [5])
+column_transform_train = ColumnTransformer(
+        [('Pclass', OneHotEncoder(categories='auto'), [0]),
+         ('Sex', OneHotEncoder(categories='auto'), [1]),
+         ('Embarked', OneHotEncoder(categories='auto'), [5])
 ], remainder='passthrough')
 
-X_train = column_transform.fit_transform(X_train)
+X_train = column_transform_train.fit_transform(X_train)
 X_train = X_train.astype(np.float64)
+
+### encoding the test data
+label_encoder_test = LabelEncoder()
+test_set[:, 0] = label_encoder_test.fit_transform(test_set[:, 0])
+test_set[:, 1] = label_encoder_test.fit_transform(test_set[:, 1])
+test_set[:, 5] = label_encoder_test.fit_transform(test_set[:, 5])
+
+column_transform_train = ColumnTransformer(
+        [('Pclass', OneHotEncoder(categories='auto'), [0]),
+         ('Sex', OneHotEncoder(categories='auto'), [1]),
+         ('Embarked', OneHotEncoder(categories='auto'), [5])
+], remainder='passthrough')
+
+test_set = column_transform_train.fit_transform(test_set)
+test_set = test_set.astype(np.float64)
 
 # removing  dummy variables to avoid the dummy variable trap 
 
@@ -75,4 +97,6 @@ regressor = DecisionTreeRegressor(criterion = 'mse',
 regressor.fit(X_train, Y_train)
 
 # predicting the results
-regressor.predict(test_set)
+prediction = regressor.predict(test_set)
+prediction = np.reshape(prediction, (-1, 1))
+np.around(prediction, 0)
